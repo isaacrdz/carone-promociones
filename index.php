24e11432d75e7f72caff529111ba5a0f8480aa04
Isaac Rdz <zack_cpp@hotmail.com>
2015-07-21 13:40:50 -0500

    first

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Promociones</title>
+  <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '744685635678152',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+</head>
+<body>
+  <h1>Hola</h1>
+</body>
+</html>
